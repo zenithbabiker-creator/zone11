8e764c7a3496de76381cdebfbf7c5219b4d98a99
zenithbabiker-creator <zenithbabiker@gmail.com>
2026-07-31 20:41:50 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,15 +61,18 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
     implementation("com.google.ar:core:1.45.0")
-    implementation("com.huawei.hmf:tasks:6.13.0.300")
-    implementation("com.huawei.android.hms:security-encrypt:6.13.0.300")
-    implementation("com.huawei.android.hms:security-ssl:6.13.0.300")
-    implementation("com.huawei.hms:network-grs:6.13.0.300")
-    implementation("com.huawei.android.hms:security-base:6.13.0.300")
+    
+    # استخدام الإصدار الذهبي الذي تم التحقق منه ونجح تحميله بنجاح تام
+    implementation("com.huawei.hmf:tasks:6.11.0.300")
+    implementation("com.huawei.android.hms:security-encrypt:6.11.0.300")
+    implementation("com.huawei.android.hms:security-ssl:6.11.0.300")
+    implementation("com.huawei.hms:network-grs:6.11.0.300")
+    implementation("com.huawei.android.hms:security-base:6.11.0.300")
     implementation("com.huawei.agconnect:agconnect-core:1.9.0.300")
     implementation("com.huawei.hms:arenginesdk:4.0.0.5")
-    implementation("com.huawei.hms:base:6.13.0.300")
-    implementation("com.huawei.hms:hatool:6.13.0.300")
+    implementation("com.huawei.hms:base:6.11.0.300")
+    implementation("com.huawei.hms:hatool:6.11.0.300")
+    
     implementation("androidx.camera:camera-camera2:1.4.0")
     implementation("androidx.camera:camera-lifecycle:1.4.0")
     implementation("androidx.camera:camera-view:1.4.0")
