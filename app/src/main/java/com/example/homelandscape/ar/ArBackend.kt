66a7881c9a39d9ef070f6cf736d0da7d6feb29a2
zenithbabiker-creator <zenithbabiker@gmail.com>
2026-08-03 11:28:20 +0200

    Delete app directory

--- app/src/main/java/com/example/homelandscape/ar/ArBackend.kt
+++
@@ -1,7 +0,0 @@
-package com.example.homelandscape.ar
-
-enum class ArBackend {
-    ARCORE,
-    HUAWEI_ARENGINE,
-    NONE,
-}
--- app/src/main/java/com/example/homelandscape/measure/MeasurementMode.kt
+++
@@ -1,6 +0,0 @@
-package com.example.homelandscape.measure
-
-enum class MeasurementMode {
-    EXCAVATION_HOLE,
-    FLAT_SURFACE,
-}
